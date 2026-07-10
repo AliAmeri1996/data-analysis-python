a77e5a67437ed5243c923049c44a5db3e5674e11
Ali Ameri <aliameri82@gmail.com>
2026-07-10 17:14:03 +0100

    add oil price analysis

--- data-analysis/oil price analysis.py
+++ data-analysis/oil price analysis.py
@@ -1,6 +1,17 @@
+
+"""yfinance automatically returns the data as a pandas DataFrame,
+so everything you do after (.head(), .describe(), .groupby(), .mean() etc.) is all standard pandas.
+ That's exactly why we imported pandas at the top."""
+
+
+
 import yfinance as yf
 import pandas as pd 
 
-oil=yf.download('CL=F', start='2020-01-01',end='2024-12-31')
-print(oil.head())
-print(oil.shape)
+oil=yf.download('CL=F', start='2020-01-01',end='2024-12-31')#CL=F is the ticker symbol for WTI Crude Oil Futures on Yahoo Finance.
+#BZ=FBrent Crude Oil
+print(oil.head())# first 5 rows (default)
+print(oil.shape)#.shape tells you the dimensions of the DataFrame — how many rows and columns you have.
+
+
+print(oil['Close'].describe())# Close is the close price of oil in that period and describe basically gives some stats abour it like mean and all
