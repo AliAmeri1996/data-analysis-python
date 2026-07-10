ba38c388ce0fa8b3d0dc0056fdfa4d1a99476a46
Ali Ameri <aliameri82@gmail.com>
2026-07-10 17:48:21 +0100

    add oil price visualisation

--- data-analysis/oil price analysis.py
+++ data-analysis/oil price analysis.py
@@ -4,7 +4,7 @@ so everything you do after (.head(), .describe(), .groupby(), .mean() etc.) is a
  That's exactly why we imported pandas at the top."""
 
 
-
+import matplotlib.pyplot as plt 
 import yfinance as yf
 import pandas as pd 
 
@@ -15,3 +15,30 @@ print(oil.shape)#.shape tells you the dimensions of the DataFrame — how many r
 
 
 print(oil['Close'].describe())# Close is the close price of oil in that period and describe basically gives some stats abour it like mean and all
+
+
+#30-day rolling average ,average closing price over the last 30 days
+oil['MA30']=oil['Close'].rolling(30).mean()
+
+# Daily % change
+oil['Daily_Return']=oil['Close'].pct_change()*100 # ok so basically when we say oil'Daily_Return'] this way we're making a column called 
+#'Daily_Return'
+
+print(oil[['Close', 'MA30', 'Daily_Return']].tail(10))
+
+
+oil['Mean'] = oil['Close'].mean()# if we want to make a column we first have to make it like this
+
+
+
+
+"""now lets visulise the data with matplotlib"""
+
+oil['MA30']=oil['Close'].rolling(30).mean()
+oil['Close'].plot(label='Close Price', figsize=(12,6))
+oil['MA30'].plot(label='30 Day MA')
+plt.title('WTI Crude Oil Price 2020-2024')
+plt.xlabel('Date')
+plt.ylabel('Price (USD)')
+plt.legend()
+plt.show()
