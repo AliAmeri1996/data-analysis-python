41adbc8a16285c534797253f73f4fdd5e7b8d4cc
Ali Ameri <aliameri82@gmail.com>
2026-07-11 19:53:15 +0100

    add gold price analysis 2

--- data-analysis/gold price analysis.py
+++ data-analysis/gold price analysis.py
@@ -1,15 +1,15 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
-import pandas as pd 
+import pandas as pd
 
 gold=yf.download('GC=F',start='2019-01-01',end='2025-01-01')
 gold.columns=['Close',"High",'Low',"Open",'Volume']
 
-#Explore the data first 
+#1-Explore the data first
 
-print("shape:", gold.shape)#tolist() just converts the columns index into a plain Python list so it prints cleanly like:
+print("shape:", gold.shape)#Number of rows and columns
+print("\nColumns:",gold.columns.tolist())#\n just adds a new line before "Columns:"tolist() just converts the columns index into a plain Python list so it prints cleanly like:
 #['Close', 'High', 'Low', 'Open', 'Volume']
-print("\nColumns:",gold.columns.tolist())#\n just adds a new line before "Columns:"
 print("\nFirst 5 rows:")
 print(gold.head())
 print("\nData types:")
@@ -18,3 +18,17 @@ print("\nSummary stats:")
 print(gold.describe())
 print("\nMissing values:")
 print(gold.isnull().sum())
+
+
+
+#2- cleaning the data file
+
+print("\nnumber of missing values:")
+print(gold.isnull().sum())
+
+print("\nnumber of duplicates:")
+print(gold.duplicated().sum())
+
+gold=gold.dropna()#removes rows with missing values
+gold = gold.drop_duplicates()
+
