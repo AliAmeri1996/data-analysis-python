935f381a85c79d086aac81d0e3cbe92e72fa022c
Ali Ameri <aliameri82@gmail.com>
2026-07-11 18:54:44 +0100

    add oil price3

--- data-analysis/oil price analysis.py
+++ data-analysis/oil price analysis.py
@@ -12,7 +12,11 @@ oil=yf.download('CL=F', start='2020-01-01',end='2024-12-31')#CL=F is the ticker 
 #BZ=FBrent Crude Oil
 oil.columns = ['Close', 'High', 'Low', 'Open', 'Volume']
 
-'''exploratory data analysis (EDA)'''
+
+
+
+
+''' 1-exploratory data analysis (EDA)= first thing to do to see what kind of data we have '''
 print(oil.columns)       # what columns exist
 print(oil.shape)         # how many rows/columns
 print(oil.head())        # first 5 rows
@@ -20,35 +24,68 @@ print(oil.dtypes)        # data types of each column
 print(oil.describe())    # summary stats
 print(oil.isnull().sum()) # any missing values?
 
-
-# print(oil['Close'].describe())# Close is the close price of oil in that period and describe basically gives some stats abour it like mean and all
-
-
-# #30-day rolling average ,average closing price over the last 30 days
-# oil['MA30']=oil['Close'].rolling(30).mean()
-
-# # Daily % change
-# oil['Daily_Return']=oil['Close'].pct_change()*100 # ok so basically when we say oil['Daily_Return'] this way we're making a column called 
-# #'Daily_Return'
-
-# print(oil[['Close', 'MA30', 'Daily_Return']].tail(10))
+oil['Mean'] = oil['Close'].mean()# if we want to make a column we first have to make it like this
 
 
-# oil['Mean'] = oil['Close'].mean()# if we want to make a column we first have to make it like this
 
+
+
+'''2- cleaning the data=Cleaning is about fixing problems in your data before you analyse it. The main things you'd check for:'''
+
+print(oil.isnull().sum())# count missing values per column
+oil.dropna()# remove rows with any missing value, no need to print 
+oil.fillna(0)# fill missing with 0,no need to print 
+oil.fillna(method='ffill')# fill with previous day's value (common in finance),no need to print 
+
+#duplicate rows 
+print(oil.duplicated().sum()) # count duplicatess
+oil.drop_duplicates()
+
+
+#Wrong data types
+print(oil.dtypes) # check types
+oil['Volume']=oil['volume'].astype(int) # convert to integer
+
+
+#renaming messy columns names 
+oil.rename(columns={'Close':'close_price'},inplace=True)\
+
+#renaming columns you dont need
+oil.drop(columns=['Volume'],inplace=True)
+
+
+
+
+
+'''3- analysing the data now '''
+
+
+#30-day rolling average ,average closing price over the last 30 days
+oil['MA30']=oil['Close'].rolling(30).mean()
+
+# Daily % change
+oil['Daily_Return']=oil['Close'].pct_change()*100 # ok so basically when we say oil['Daily_Return'] this way we're making a column called 
+#'Daily_Return'
+print(oil[['Close', 'MA30', 'Daily_Return']].tail(10))
 
 oil_price_over80=oil[oil['Close']>80]# so this is filtering the closes that are over 80 , the column is close
 print(oil_price_over80)
 
-
-# 3. Combining two conditions - price over 80 AND volume over 500k
+# 3. Combining two conditions - # 4. How many days was oil above $80
+# AND volume over 500k
 oil_busy = oil[(oil['Close'] > 80) & (oil['Volume'] > 500000)]
-print(oil_busy)
-print(oil.dtypes)
+print("Days above $80:",len(oil_busy))
 
 
+# 2. Volatility per year (how much prices jumped around)
+print(oil.groupby('Year')['Close'].std())
 
 
+# 3. Best and worst trading days
+oil['Daily_Return'] = oil['Close'].pct_change() * 100
+print("best day in oil market:", oil["Close"].max())
+print("worst close day in the oil market:",oil['Close'].min())
+print("best day date:", oil['Daily_Return'].idmax())
 
 
 
--- data-analysis/practice.py
+++ data-analysis/practice.py
@@ -4,4 +4,6 @@ import pandas as pd
 
 
 gold=yf.download('GC=F', start='2020-01-01',end='2022-01-01')
-print(gold.columns)
+print(gold.isnull().sum())
+print("best day in oil market:", gold["Close"].max())
+print("worst close day in the oil market:",gold['Close'].min())
