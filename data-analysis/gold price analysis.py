f0675f22462638ff051ace90aaeafefb1d1d41c7
Ali Ameri <aliameri82@gmail.com>
2026-07-12 20:36:23 +0100

    add gold price analysis 3

--- data-analysis/gold price analysis.py
+++ data-analysis/gold price analysis.py
@@ -32,3 +32,21 @@ print(gold.duplicated().sum())
 gold=gold.dropna()#removes rows with missing values
 gold = gold.drop_duplicates()
 
+
+#3-Analysis 
+
+print("\n Average price per year")
+gold['Year']=gold.index.year #It creates a new column called Year by extracting just the year from the date index.
+average_price_per_year=gold.groupby('Year')['Close'].mean()
+print(average_price_per_year)
+#groupby('Year') splits the data into groups — one group for each year (2020, 2021, 2022 etc). Then .mean() calculates the average Close price for each group separately.
+
+
+print("\n best and worst day")
+gold["Daily_Return"]=gold['Close'].pct_change()*100
+best_day=gold["Daily_Return"].max()
+worst_day=gold["Daily_Return"].min()
+best_day_date = gold['Daily_Return'].idxmax()
+worst_day_date = gold['Daily_Return'].idxmin()
+print("\nbest day:",best_day,"\nworst day:",worst_day,"\n best day date:",best_day_date,"\n worst day date:",worst_day_date)
+
